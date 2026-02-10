e1d8f06807e7b4dcca3bb623f4adf23cddc947f3
LudivineLeLan <lelanludivine@gmail.com>
2026-02-10 16:16:16 +0100

    update book status method added

--- src/controllers/userbook.controller.js
+++ src/controllers/userbook.controller.js
@@ -120,6 +120,49 @@ async search(req, res) {
     console.error(error);
     return res.status(500).json({ message: "Erreur serveur" });
   }
+},
+
+async updateReadStatus(req, res) {
+  try {
+    const { userId, bookId } = req.params;
+    const { toRead } = req.body;
+
+    if (!userId || !bookId) {
+      return res.status(400).json({ message: "userId et bookId sont requis" });
+    }
+
+    if (typeof toRead !== "boolean") {
+      return res.status(400).json({ message: "toRead doit être un booléen" });
+    }
+
+    const userBook = await UserBook.findOne({
+      where: {
+        user_id: userId,
+        book_id: bookId,
+      },
+    });
+
+    if (!userBook) {
+      return res.status(404).json({
+        message: "Livre non trouvé dans la booklist de l'utilisateur.",
+      });
+    }
+
+    userBook.toRead = toRead;
+    await userBook.save();
+
+    return res.status(200).json({
+      message: "Statut de lecture mis à jour avec succès.",
+      data: userBook,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      error: "Erreur serveur",
+      message: error.message,
+    });
+  }
 }
+
 
 };
