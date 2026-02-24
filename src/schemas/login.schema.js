3804308105ace5d5eb48ac34dc3b637618ded630
LudivineLeLan <lelanludivine@gmail.com>
2026-02-24 16:57:42 +0100

    login schema

--- src/schemas/login.schema.js
+++ src/schemas/login.schema.js
@@ -1,6 +1,12 @@
 import Joi from "joi";
 
 export const loginSchema = Joi.object({
-  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr'] } }), 
-  password: Joi.string().required()
-});
+	email: Joi.string().email().lowercase().trim().required().messages({
+		"string.email": "Email invalide",
+		"string.empty": "L'email est obligatoire",
+	}),
+
+	password: Joi.string().required().messages({
+		"string.empty": "Le mot de passe est obligatoire",
+	}),
+}).strict();
