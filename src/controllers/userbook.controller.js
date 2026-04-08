13f1874e4e7d9d7c5f716c2d897024ab429bfd7e
LudivineLeLan <lelanludivine@gmail.com>
2026-04-08 17:56:10 +0200

    feat: added method to remove book from library

--- src/controllers/userbook.controller.js
+++ src/controllers/userbook.controller.js
@@ -151,4 +151,28 @@ export const userBookController = {
 			return res.status(500).json({ message: "Erreur serveur" });
 		}
 	},
+
+	// Supprimer un livre de la bibliothèque
+	async removeBookFromUserList(req, res) {
+		try {
+			const userId = req.user.id;
+			const { bookId } = req.params;
+
+			const deleted = await UserBook.destroy({
+				where: { userId, bookId },
+			});
+
+			if (!deleted)
+				return res
+					.status(404)
+					.json({ message: "Livre non trouvé dans votre bibliothèque." });
+
+			return res
+				.status(200)
+				.json({ message: "Livre supprimé de votre bibliothèque." });
+		} catch (error) {
+			console.error(error);
+			return res.status(500).json({ message: "Erreur serveur" });
+		}
+	},
 };
