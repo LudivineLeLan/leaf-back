ff4c6007f0b503a0a0e9639600caf93146bc2cc7
LudivineLeLan <lelanludivine@gmail.com>
2026-04-13 16:14:54 +0200

    feat: update book & serie model

--- src/models/book.model.js
+++ src/models/book.model.js
@@ -1,40 +1,47 @@
 import { DataTypes, Model } from "sequelize";
-import { sequelize } from './sequelize.client.js';
-
+import { sequelize } from "./sequelize.client.js";
 
 export class Book extends Model {}
 
 Book.init(
-  {
-    googleBooksId: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      unique: true
-    },
-    title: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    releaseDate: {
-      type: DataTypes.DATEONLY,
-      allowNull: true
-    },
-    cover: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    synopsis: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    serieId: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    }
-  },
-  {
-    sequelize,
-    tableName: 'books',
-    underscored: true
-  }
+	{
+		googleBooksId: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+			unique: true,
+		},
+		title: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+		},
+		releaseDate: {
+			type: DataTypes.DATEONLY,
+			allowNull: true,
+		},
+		cover: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		synopsis: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+		},
+		serieId: {
+			type: DataTypes.UUID,
+			allowNull: true,
+		},
+		seriesPosition: {
+			type: DataTypes.INTEGER,
+			allowNull: true,
+		},
+		seriesDetected: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: false,
+		},
+	},
+	{
+		sequelize,
+		tableName: "books",
+		underscored: true,
+	},
 );
--- src/models/serie.model.js
+++ src/models/serie.model.js
@@ -1,39 +1,40 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "./sequelize.client.js";
 
-export class Serie extends Model { }
+export class Serie extends Model {}
 
 Serie.init(
-   {
-      title: {
-         type: DataTypes.STRING(255), 
-         allowNull: false,
-      },
-      description: {
-         type: DataTypes.TEXT, 
-         allowNull: true
-      },
-      type: {
-         type: DataTypes.ENUM('manga', 'novel', 'comic'),
-         allowNull: false,
-         defaultValue: 'manga'
-      },
-      total_volumes: {
-         type: DataTypes.INTEGER,
-         allowNull: true 
-      },
-      status: {
-         type: DataTypes.ENUM('ongoing', 'completed', 'hiatus'),
-         allowNull: false,
-         defaultValue: 'ongoing'
-      },
-      external_api_id: {
-         type: DataTypes.STRING, 
-         unique: true
-      }
-   },
-   {
-      sequelize,
-      tableName: "series",
-   }
+	{
+		name: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+			normalizedName: DataTypes.STRING,
+		},
+		description: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+		},
+		type: {
+			type: DataTypes.ENUM("manga", "novel", "comic"),
+			allowNull: false,
+			defaultValue: "manga",
+		},
+		total_volumes: {
+			type: DataTypes.INTEGER,
+			allowNull: true,
+		},
+		status: {
+			type: DataTypes.ENUM("ongoing", "completed", "hiatus"),
+			allowNull: false,
+			defaultValue: "ongoing",
+		},
+		external_api_id: {
+			type: DataTypes.STRING,
+			unique: true,
+		},
+	},
+	{
+		sequelize,
+		tableName: "series",
+	},
 );
