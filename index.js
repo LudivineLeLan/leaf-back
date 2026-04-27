45da94a5785db5f59596cb5d8f19c3662fba5063
LudivineLeLan <lelanludivine@gmail.com>
2026-04-27 11:57:25 +0200

    fix: allowed cookies for cors

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	}),
+);
 
 app.use(cookieParser());
 
