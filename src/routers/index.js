45efd537a00addcc14419bd9d8879945b66af681
LudivineLeLan <lelanludivine@gmail.com>
2026-04-30 13:19:55 +0200

    fix: typo in route

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,6 +5,6 @@ import { authRouter } from "./auth.router.js";
 
 export const apiRouter = Router();
 
-apiRouter.use("/librairy", userBookRouter);
+apiRouter.use("/library", userBookRouter);
 apiRouter.use("/books", bookRouter);
 apiRouter.use("/auth", authRouter);
