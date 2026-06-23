d7af1672726ad92623684324f8c51bf2e80ac784
LudivineLeLan <lelanludivine@gmail.com>
2026-06-23 09:33:38 +0200

    code review : Joi schemas

--- src/schemas/login.schema.js
+++ src/schemas/login.schema.js
@@ -4,9 +4,13 @@ export const loginSchema = Joi.object({
 	email: Joi.string().email().lowercase().trim().required().messages({
 		"string.email": "Email invalide",
 		"string.empty": "L'email est obligatoire",
+		"any.required": "L'email est obligatoire",
 	}),
 
 	password: Joi.string().required().messages({
 		"string.empty": "Le mot de passe est obligatoire",
+		"any.required": "Le mot de passe est obligatoire",
 	}),
-}).strict();
+})
+	.unknown(false) //only accepts allowed fields with right type
+	.strict(); //to convert value
--- src/schemas/register.schema.js
+++ src/schemas/register.schema.js
@@ -1,35 +1,26 @@
 import Joi from "joi";
 
-export const registerSchema = Joi.object({
-	username: Joi.string().min(2).max(30).trim().required().messages({
-		"string.empty": "Le nom d'utilisateur est obligatoire",
-		"string.min": "Le nom d'utilisateur doit contenir au moins 2 caractères",
-		"string.max": "Le nom d'utilisateur ne peut pas dépasser 30 caractères",
-	}),
+Joi.object({
+	username: Joi.string()
+		.min(2)
+		.max(30)
+		.pattern(/^[\p{L}\p{N}_-]+$/u)
+		.trim()
+		.required(),
 
-	email: Joi.string().email().lowercase().trim().required().messages({
-		"string.email": "Email invalide",
-		"string.empty": "L'email est obligatoire",
-	}),
+	email: Joi.string().email().lowercase().trim().required(),
 
 	password: Joi.string()
-		.pattern(
-			/^(?=.*\p{L})(?=.*\p{N})[\p{L}\p{N}!@#$%^&*()_+\-=[\]{}|;:'",.<>?/]{8,30}$/u,
-		)
-		.required()
-		.messages({
-			"string.pattern.base":
-				"Le mot de passe doit contenir 8 à 30 caractères, avec au moins une lettre et un chiffre",
-			"string.empty": "Le mot de passe est obligatoire",
-		}),
+		.min(8)
+		.max(30)
+		.pattern(/(?=.*\p{L})(?=.*\p{N})/u)
+		.required(),
 
-	avatar: Joi.alternatives()
-		.try(Joi.string().uri(), Joi.string().pattern(/\.(png|jpg|jpeg|webp)$/i))
+	avatar: Joi.string()
+		.uri()
+		.pattern(/\.(png|jpg|jpeg|webp)$/i)
 		.allow("")
-		.optional()
-		.messages({
-			"string.uri": "L'avatar doit être une URL valide",
-			"string.pattern.base":
-				"Le fichier avatar doit être au format png, jpg, jpeg ou webp",
-		}),
-}).strict();
+		.optional(),
+})
+	.unknown(false)
+	.strict();
